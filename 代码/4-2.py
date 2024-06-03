49849d572090594adeb1dd685a0ba3e29036eb63
Eddybud88 <zyx030228@163.com>
2024-06-03 22:48:34 +0800

    Create 4-2.py

---
+++ 代码/4-2.py
@@ -0,0 +1,10 @@
+animals = ['dog' , 'cat' , 'fox']
+for animal in animals:
+  print("A "+ animal + " have a tail.")
+print("All these animals have tails!")
+
+输出：
+A dog have a tail.
+A cat have a tail.
+A fox have a tail.
+All these animals have tails!
