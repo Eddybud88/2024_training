a2b6aa7e708057536cea2ad4a3626d7c4bcbfeab
Eddybud88 <zyx030228@163.com>
2024-06-03 11:45:52 +0800

    Create 3-3.py

---
+++ 代码/3-3.py
@@ -0,0 +1,11 @@
+commute_modes = ["Honda motorcycle", "Ferrari car", "Yadi Bike", "Boeing Plane"]
+print("I want to own a " + commute_modes[0])
+print("I want to own a " + commute_modes[1])
+print("I want to own a " + commute_modes[2])
+print("I want to own a " + commute_modes[3])
+
+输出：
+I want to own a Honda motorcycle
+I want to own a Ferrari car
+I want to own a Yadi Bike
+I want to own a Boeing Plane
