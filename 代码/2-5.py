061bb96f4fd6e5d4792307fe96f86d874c944b3d
Eddybud88 <zyx030228@163.com>
2024-06-03 11:15:09 +0800

    Update 2-5.py

--- 代码/2-5.py
+++ 代码/2-5.py
@@ -1,2 +1,3 @@
-sentence = 'Albert Einstein once said, “A person who never made a mistake never tried anything new.”'
+sentence = '\t\tAlbert Einstein once said, “A person who never made a mistake never tried anything new.”'
 print(sentence)
+  Albert Einstein once said, “A person who never made a mistake never tried anything new.”
