59d6da3187d755ebcdb2e6e339efbdcbf16a740d
Eddybud88 <zyx030228@163.com>
2024-06-03 22:45:45 +0800

    Create 4-1.py

---
+++ 代码/4-1.py
@@ -0,0 +1,15 @@
+pizzas = ['Dameile' , 'Bishengke' , 'MrPizza']
+for pizza in pizzas:
+  print(pizza)
+for pizza in pizzas:
+  print("I like " +  pizza + " pizza")
+print('I really love pizza!')
+
+输出：
+Dameile
+Bishengke
+MrPizza
+I like Dameile pizza
+I like Bishengke pizza
+I like MrPizza pizza
+I really love pizza!
