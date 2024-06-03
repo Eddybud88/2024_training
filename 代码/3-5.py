070caaf07703aac3b59b95b8e346aa7f63d67228
Eddybud88 <zyx030228@163.com>
2024-06-03 11:53:24 +0800

    Create 3-5.py

---
+++ 代码/3-5.py
@@ -0,0 +1,18 @@
+names = ['Napolean' , 'Stalin' , 'Lennon']
+print("Dear " + names[0] + ", I would like to invite you to have dinner with me.")
+print("Dear " + names[1] + ", I would like to invite you to have dinner with me.")
+print("Dear " + names[2] + ", I would like to invite you to have dinner with me.")
+print(names[1] + " couldn't attend")
+names[1] = 'Judy Foster'
+print("Dear " + names[0] + ", I would like to invite you to have dinner with me.")
+print("Dear " + names[1] + ", I would like to invite you to have dinner with me.")
+print("Dear " + names[2] + ", I would like to invite you to have dinner with me.")
+
+输出：
+Dear Napolean, I would like to invite you to have dinner with me.
+Dear Stalin, I would like to invite you to have dinner with me.
+Dear Lennon, I would like to invite you to have dinner with me.
+Stalin couldn't attend
+Dear Napolean, I would like to invite you to have dinner with me.
+Dear Judy Foster, I would like to invite you to have dinner with me.
+Dear Lennon, I would like to invite you to have dinner with me.
