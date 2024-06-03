edcb9ae03583223dd87a68f1f5a2bb22fee3c5e2
Eddybud88 <zyx030228@163.com>
2024-06-03 11:12:22 +0800

    Create 2-4.py

---
+++ 代码/2-4.py
@@ -0,0 +1,9 @@
+name = "Cristiano Ronaldo Dos Santos Aveiro"
+print(name.title())  # 首字母大写
+print(name.upper()) # 全大写
+print(name.lower()) # 全小写
+
+输出：
+Cristiano Ronaldo Dos Santos Aveiro
+CRISTIANO RONALDO DOS SANTOS AVEIRO
+cristiano ronaldo dos santos aveiro
