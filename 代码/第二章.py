76f4f91ff8981a96b0818e24d465ac1ac6c61c2f
Eddybud88 <zyx030228@163.com>
2024-06-03 09:49:19 +0800

    Update 第二章.py

--- 代码/第二章.py
+++ 代码/第二章.py
@@ -1,4 +1,25 @@
 # 2.1 hello world
-message= "hello world!";
+message = "hello world!";
 print(message)
+
+hello world!
+
+# 2.2 变量
+message= "hello world!";  
+print(message)  
+
+hello world!
+
+# 2.3 字符串  
+name = "ada wong"
+print(name.title())  # 首字母大写
+
+Ada Wong
+
+name  =  "Ada  Wong"
+print(name.upper()) # 全大写
+print(name.lower()) # 全小写
+
+ADA WONG
+ada wong
 
