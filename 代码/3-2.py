47fc420923244b629fa10a0bd082b74eb6e6b7a6
Eddybud88 <zyx030228@163.com>
2024-06-03 11:43:00 +0800

    Create 3-2.py

---
+++ 代码/3-2.py
@@ -0,0 +1,9 @@
+name = ['Eddy' , 'Kurt' , 'Rose']
+print("Hello, " + name[0] + " , hope you a nice day!")
+print("Hello, " + name[1] + " , hope you a nice day!")
+print("Hello, " + name[2] + " , hope you a nice day!")
+
+输出：
+Hello, Eddy , hope you a nice day!
+Hello, Kurt , hope you a nice day!
+Hello, Rose , hope you a nice day!
