e683178aa55b76a507d4d9d9bc29a5079ef99613
Eddybud88 <zyx030228@163.com>
2024-06-03 11:54:45 +0800

    Create 3-6.py

---
+++ 代码/3-6.py
@@ -0,0 +1,10 @@
+names = ['Napolean' , 'Stalin' , 'Lennon']
+print("Dear " + names[0] + ", I would like to invite you to have dinner with me.")
+print("Dear " + names[1] + ", I would like to invite you to have dinner with me.")
+print("Dear " + names[2] + ", I would like to invite you to have dinner with me.")
+print(names[1] + " couldn't attend")
+names[1] = 'Judy Foster'
+print("Dear " + names[0] + ", I would like to invite you to have dinner with me.")
+print("Dear " + names[1] + ", I would like to invite you to have dinner with me.")
+print("Dear " + names[2] + ", I would like to invite you to have dinner with me.")
+print("I have found a larger table.")
