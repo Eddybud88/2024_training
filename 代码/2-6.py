dcff5807975e4253a38e6572179629aab6a6fef7
Eddybud88 <zyx030228@163.com>
2024-06-03 11:20:05 +0800

    Create 2-6.py

---
+++ 代码/2-6.py
@@ -0,0 +1,6 @@
+famous_person = "Albert Einstein"
+sentence = "  " + famous_person + ' once said, “A person who never made a mistake never tried anything new."'
+print(sentence)
+
+输出
+  Albert Einstein once said, “A person who never made a mistake never tried anything new".
