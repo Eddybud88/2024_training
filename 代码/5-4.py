04168229e2a2406149e8d9e854f0f12cab5d8a66
Eddybud88 <zyx030228@163.com>
2024-06-04 16:30:28 +0800

    Update 5-4.py

--- 代码/5-4.py
+++ 代码/5-4.py
@@ -1,1 +1,14 @@
+alien_color = 'green'
+if alien_color == 'green':
+  print("You get 5 points!")
+else:
+  print("You get 10 points!")
+alien_color = 'red'
+if alien_color == 'green':
+  print("You get 5 points!")
+else:
+  print("You get 10 points!")
 
+输出：
+You get 5 points!
+You get 10 points!
