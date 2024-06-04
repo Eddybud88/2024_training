e8e572d4307f5bb405fc82c322f52aee230a8875
Eddybud88 <zyx030228@163.com>
2024-06-04 16:55:33 +0800

    Create 5-11.py

---
+++ 代码/5-11.py
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]  
+
+for number in numbers:   
+    if number == 1:  
+        print("1st")  
+    elif number == 2:  
+        print("2nd")  
+    elif number == 3:  
+        print("3rd")  
+    else:   
+       print(str(number) + "th")
+
+  输出：
+1st
+2nd
+3rd
+4th
+5th
+6th
+7th
+8th
+9th
