d88e4ebfb489ec60a2a9419e280969679a159780
Eddybud88 <zyx030228@163.com>
2024-06-04 16:52:46 +0800

    Update 5-10.py

--- 代码/5-10.py
+++ 代码/5-10.py
@@ -3,12 +3,14 @@ current_users = ['Eric', 'Alice', 'Bob', 'admin', 'Charlie']
 new_users = ['David', 'admin', 'Emily', 'Bob', 'Frank']  
  
 for new_user in new_users:  
-    if new_user in current_users:  
+    lowercase_new_user = new_user.lower()  
+     
+    if lowercase_new_user in [user.lower() for user in current_users]:    
         print(f"Username '{new_user}' is already taken. Please enter a different username.")  
     else:  
         print(f"Username '{new_user}' is available.")
 
-  输出：
+输出：
 Username 'David' is available.
 Username 'admin' is already taken. Please enter a different username.
 Username 'Emily' is available.
