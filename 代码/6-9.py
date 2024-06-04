ec03f2e9e28482fa9427cb5cb06f3998c9dab6fb
Eddybud88 <zyx030228@163.com>
2024-06-04 22:44:24 +0800

    Create 6-9.py

---
+++ 代码/6-9.py
@@ -0,0 +1,13 @@
+favorite_places = {  
+    'Alice': ['Paris', 'Tokyo', 'London'],  
+    'Bob': ['Hawaii', 'Rome'],  
+    'Charlie': ['New York', 'Sydney']  
+}  
+  
+for name, places in favorite_places.items():  
+    print(f'{name} 的喜欢的地方有: {places}')
+
+输出：
+Alice 的喜欢的地方有: ['Paris', 'Tokyo', 'London']  
+Bob 的喜欢的地方有: ['Hawaii', 'Rome']  
+Charlie 的喜欢的地方有: ['New York', 'Sydney']
