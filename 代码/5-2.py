9766f807c59740d78aeed369d0a63179da627c28
Eddybud88 <zyx030228@163.com>
2024-06-04 16:21:28 +0800

    Update 5-2.py

--- 代码/5-2.py
+++ 代码/5-2.py
@@ -16,3 +16,24 @@ age1 = 23
 age2 = 25
 print(age1 == 22 and age2 == 22)
 print(age1 == 23 or age2 ==23)
+names = ['Napolean' , 'Stalin' , 'Lennon']
+name1 = 'Lennon'
+name2 = 'Lenin'
+if name1 in names:
+  print("Yeah!")
+if name2 not in names:
+  print("Oops!")
+
+输出：
+Not True
+Alright
+False
+True
+True
+False
+False
+False
+False
+True
+Yeah!
+Oops!
