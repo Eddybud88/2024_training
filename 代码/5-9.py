2aa7746ec07c390a8cb313bef3e6443a452d858c
Eddybud88 <zyx030228@163.com>
2024-06-04 16:47:54 +0800

    Create 5-9.py

---
+++ 代码/5-9.py
@@ -0,0 +1,8 @@
+usernames = ['Eric', 'Alice', 'Bob', 'admin', 'Charlie']  
+usernames.clear() 
+  
+if not usernames:  
+    print("We need to find some users!") 
+
+输出：
+We need to find some users!
