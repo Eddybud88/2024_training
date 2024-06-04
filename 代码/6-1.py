dbe4cd0312ab4571b624d4a1d548099552d69bbd
Eddybud88 <zyx030228@163.com>
2024-06-04 17:04:57 +0800

    Create 6-1.py

---
+++ 代码/6-1.py
@@ -0,0 +1,15 @@
+person_info = {  
+    'first_name': 'Eddy',  
+    'last_name': 'Bud',  
+    'age': 21,  
+    'city': 'Wuhan'  
+}  
+
+for key, value in person_info.items():  
+    print(f'{key}: {value}')  
+
+输出：
+first_name: Eddy
+last_name: Bud
+age: 21
+city: Wuhan
