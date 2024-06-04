d87bd3e92546a284f4b5f68f4c3294384b9f9003
Eddybud88 <zyx030228@163.com>
2024-06-04 16:10:37 +0800

    Create 5-1.py

---
+++ 代码/5-1.py
@@ -0,0 +1,61 @@
+car = 'subaru' 
+print("Is car == 'subaru'? I predict True.") 
+print(car == 'subaru') 
+car = 'Honda'
+print("\nIs car == 'audi'? I predict False.") 
+print(car == 'audi')
+car = 'Ferarri'
+print("\nIs car == 'audi'? I predict False.") 
+print(car == 'audi')
+car = 'Ferarri'
+print("\nIs car == 'Ferarri'? I predict True.") 
+print(car == 'Ferarri')
+car = 'Mecerdes'
+print("\nIs car == 'Ferarri'? I predict False.") 
+print(car == 'Ferarri')
+car = 'Mecerdes'
+print("\nIs car == 'Mecerdes'? I predict True.") 
+print(car == 'Mecerdes')
+car = 'RR'
+print("\nIs car == 'Mecerdes'? I predict False.") 
+print(car == 'Mecerdes')
+car = 'RR'
+print("\nIs car == 'RR'? I predict True.") 
+print(car == 'RR')
+car = 'Boeing'
+print("\nIs car == 'Airbus'? I predict False.") 
+print(car == 'Airbus')
+car = 'Boeing'
+print("\nIs car == 'Boeing'? I predict True.") 
+print(car == 'Boeing')
+
+输出：
+Is car == 'subaru'? I predict True.
+True
+
+Is car == 'audi'? I predict False.
+False
+
+Is car == 'audi'? I predict False.
+False
+
+Is car == 'Ferarri'? I predict True.
+True
+
+Is car == 'Ferarri'? I predict False.
+False
+
+Is car == 'Mecerdes'? I predict True.
+True
+
+Is car == 'Mecerdes'? I predict False.
+False
+
+Is car == 'RR'? I predict True.
+True
+
+Is car == 'Airbus'? I predict False.
+False
+
+Is car == 'Boeing'? I predict True.
+True
