221277faac37684240766f4bc53fcd83d768c53a
Eddybud88 <zyx030228@163.com>
2024-06-04 17:19:39 +0800

    Create 6-3.py

---
+++ 代码/6-3.py
@@ -0,0 +1,29 @@
+vocabulary = {  
+    'variable': '在编程中，变量是用于存储数据的容器，这些数据可以是数字、文本、列表等。',  
+    'function': '函数是一段可以重复使用的代码块，它执行特定的任务并可能返回结果。',  
+    'loop': '循环是一种控制结构，它允许代码块重复执行，直到满足某个条件为止。',  
+    'condition': '条件语句用于根据特定条件执行不同的代码块，如 if、elif 和 else。',  
+    'list': '列表是 Python 中的一种数据结构，用于存储一系列有序的元素，这些元素可以是不同的数据类型。'  
+}  
+  
+print("variable:", vocabulary['variable'])  
+print()    
+print("function:", vocabulary['function'])  
+print()  
+print("loop:", vocabulary['loop'])  
+print()  
+print("condition:", vocabulary['condition'])  
+print()  
+print("list:", vocabulary['list'])  
+print()
+
+输出：
+variable: 在编程中，变量是用于存储数据的容器，这些数据可以是数字、文本、列表等。
+
+function: 函数是一段可以重复使用的代码块，它执行特定的任务并可能返回结果。
+
+loop: 循环是一种控制结构，它允许代码块重复执行，直到满足某个条件为止。
+
+condition: 条件语句用于根据特定条件执行不同的代码块，如 if、elif 和 else。
+
+list: 列表是 Python 中的一种数据结构，用于存储一系列有序的元素，这些元素可以是不同的数据类型。
