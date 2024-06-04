46fb794c20d0fb4eb6207f8e2ecc3802f1e94bad
Eddybud88 <zyx030228@163.com>
2024-06-04 15:50:07 +0800

    Update 4-11.py

--- 代码/4-11.py
+++ 代码/4-11.py
@@ -1,2 +1,22 @@
 pizzas = ['Dameile' , 'Bishengke' , 'MrPizza']
-friend_pizza = pizza[:]
+friend_pizza = pizzas[:]
+pizzas.append('Haokelai')
+friend_pizza.append('KFC')
+print("My favorite pizzas are:")
+for pizza in pizzas:
+  print(pizza)
+print("My friend’s favorite pizzas are:")
+for pizza in friend_pizza:
+  print(pizza)
+
+输出：
+My favorite pizzas are:
+Dameile
+Bishengke
+MrPizza
+Haokelai
+My friend’s favorite pizzas are:
+Dameile
+Bishengke
+MrPizza
+KFC
