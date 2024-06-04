33a93e871c681fd67530bcaaa4b6ffb43059b907
Eddybud88 <zyx030228@163.com>
2024-06-04 16:39:34 +0800

    Create 5-7.py

---
+++ 代码/5-7.py
@@ -0,0 +1,16 @@
+favorite_fruits = ['melon' , 'apple' , 'orange']
+if 'apple' in favorite_fruits:
+  print("You really like apple")
+if 'pear' in favorite_fruits:
+  print("You really like pear")
+if 'melon' in favorite_fruits:
+  print("You really like melon")
+if 'banana' in favorite_fruits:
+  print("You really like banana")
+if 'orange' in favorite_fruits:
+  print("You really like orange")
+
+输出：
+You really like apple
+You really like melon
+You really like orange
