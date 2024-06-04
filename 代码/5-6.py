446ff879e1bb3ac211c4fe0a89915998738055d6
Eddybud88 <zyx030228@163.com>
2024-06-04 16:36:51 +0800

    Create 5-6.py

---
+++ 代码/5-6.py
@@ -0,0 +1,16 @@
+age = 21
+if age < 2:
+  print("婴儿")
+elif age < 4:
+  print("在蹒跚学步")
+elif age < 13:
+  print("是儿童")
+elif age < 20:
+  print("是青少年")
+elif age < 65:
+  print("是成年人")
+elif age >= 65:
+  print("是老年人")
+
+输出：
+是成年人
