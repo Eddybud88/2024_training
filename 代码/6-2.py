b85f8efed56dac1e780ff9e5ae2b26e23c670d04
Eddybud88 <zyx030228@163.com>
2024-06-04 17:07:45 +0800

    Create 6-2.py

---
+++ 代码/6-2.py
@@ -0,0 +1,17 @@
+favorite_numbers = {  
+    'Alice': 9,  
+    'Bob': 153,  
+    'Charlie': 432,  
+    'David': 11,  
+    'George': 23  
+}  
+  
+for name, number in favorite_numbers.items():  
+    print(f"{name} 喜欢的数字是 {number}")
+
+输出：
+Alice 喜欢的数字是 9
+Bob 喜欢的数字是 153
+Charlie 喜欢的数字是 432
+David 喜欢的数字是 11
+George 喜欢的数字是 23
