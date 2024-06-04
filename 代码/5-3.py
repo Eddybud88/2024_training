0eb24b841c1c854fbe625abc1c4a6bca0ac33db2
Eddybud88 <zyx030228@163.com>
2024-06-04 16:28:22 +0800

    Create 5-3.py

---
+++ 代码/5-3.py
@@ -0,0 +1,12 @@
+alien_color = 'green'
+if alien_color == 'green':
+  print("You get 5 points!")
+alien_color = 'red'
+if alien_color == 'green':
+  print("You get 5 points!")
+else:
+  print("Sorry!")
+
+输出：
+You get 5 points!
+Sorry!
