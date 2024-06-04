7ff4c12e2e356e331a3456e1410ba0b00d93b01f
Eddybud88 <zyx030228@163.com>
2024-06-04 16:18:30 +0800

    Update 5-2.py

--- 代码/5-2.py
+++ 代码/5-2.py
@@ -1,1 +1,18 @@
-
+car = 'RR'
+if car != 'Airbus':
+  print("Not True")
+plane = 'Airbus'
+if plane == 'Airbus':
+  print("Alright")
+machine = 'Boeing'
+print(machine == 'boeing')
+print(machine.lower() == 'boeing')
+age = 22
+print(age == 22)
+print(age != 22)
+print(age <= 9)
+print(age >= 25)
+age1 = 23
+age2 = 25
+print(age1 == 22 and age2 == 22)
+print(age1 == 23 or age2 ==23)
