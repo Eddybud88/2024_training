ca24ed22171134ea7656f72ed49049b37f3441be
Eddybud88 <zyx030228@163.com>
2024-06-05 11:18:30 +0800

    Create 8-2.py

---
+++ 代码/8-2.py
@@ -0,0 +1,8 @@
+def favorite_book(title):  
+    print(f"One of my favorite books is {title}.")  
+  
+# 调用函数并传递一本图书的名称作为实参  
+favorite_book("Alice in Wonderland")
+
+输出：
+One of my favorite books is Alice in Wonderland.
